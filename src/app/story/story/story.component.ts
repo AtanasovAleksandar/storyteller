eedd1303d6d446ccaed4239de5a72c8b93324da9
Aleksandar Atanasov <aleksandar.atanasov@it-labs.com>
2020-02-04 23:12:53 +0100

    change

--- src/app/story/story/story.component.ts
+++ src/app/story/story/story.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -9,6 +9,8 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 })
 export class StoryComponent implements OnInit {
   saveText: boolean = true;
+
+  done = []
 
   story = [
     { src: '../../../assets/img/Screenshot_1.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 1 },
@@ -27,7 +29,24 @@ export class StoryComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.story, event.previousIndex, event.currentIndex);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+                        this.story = [
+                          { src: '../../../assets/img/Screenshot_1.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 1 },
+                          { src: '../../../assets/img/Screenshot_2.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 2 },
+                          { src: '../../../assets/img/Screenshot_3.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 3 },
+                          { src: '../../../assets/img/Screenshot_4.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 4 },
+                          { src: '../../../assets/img/Screenshot_5.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 5 },
+                          { src: '../../../assets/img/Screenshot_6.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 6 },
+                          { src: '../../../assets/img/Screenshot_7.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 7 },
+                          { src: '../../../assets/img/Screenshot_8.png', text: 'A short walk away, Gemma brushed past a prickly bush and followed a small break in the twisted liana vines. She came to a wall of leaves and grew excited at the sound of movement ahead. She often felt like she could sense when adventure was near. In fact, she felt it almost every single day, and for the past year, she had been wrong roughly every single time.', number: 8 }
+                        ]
+    }
   }
 
   save() {
@@ -40,5 +59,9 @@ export class StoryComponent implements OnInit {
 
   edit() {
     this.saveText = false;
+  }
+
+  delete(id) {
+    this.done.splice(id, 1);
   }
 }
